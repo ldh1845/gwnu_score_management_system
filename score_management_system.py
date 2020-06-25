ef187b5fd18bda043fef93218e0583132eb49c61
ldh <ldh1845@naver.com>
2020-06-25 16:25:42 +0900

    scoremanagement 기능구현

--- score_management_system.py
+++ score_management_system.py
@@ -2,9 +2,9 @@ class ScoreManagementSystem:
     def __init__(self):
         pass
 
-    def read(self, score_data_file):
-        with = open(score_data_file, 'rt', encording='utf-8') as fo:
-            lines = fo.readlines()
+        def read(self, score_data_file):
+             with open(score_data_file, 'rt', encoding = 'utf-8') as fo:
+            data = fo.read()
+            lines = data.strip().split('\n')
 
-        return len(lines)
-        
+            return len(lines)
--- test_score_management_system.py
+++ test_score_management_system.py
@@ -1,5 +1,8 @@
 import unittest
 from score_management_system import ScoreManagementSystem
+from unittest.mock import Mock
+from unittest.mock import patch
+from unittest.mock import mock_open
 
 class TestScoreManagementSystem(unittest.TestCase):
  
@@ -9,14 +12,30 @@ class TestScoreManagementSystem(unittest.TestCase):
 
 
 
-        def test_read_1(self): #½ºÄÚ¾îÁ¤º¸
-            cms = ScoreManagementSystem()
-            self.assertEqual(1, cms.read('score.csv'))
+        def test_read_1(self): 
+            m_open = mock_open(read_data="1,±è±è±è,90,70,80\n")
+
+            with patch('score_management_ststem.open', m_open):
+                 cms = ScoreManagementSystem()
+                 self.assertEqual(1, cms.read('score.csv'))
+
+            m_open.assert_called_with('score.csv','rt', encoding="utf-8")
+
 
         def test_read_2(self):
+            m_open = mock_open(read_data="1,±è±è±è,90,70,80\n2,³ª³ª³ª,80,95,90")
+
+        with patch('car_management_ststem.open', m_open):
             cms = ScoreManagementSystem()
             self.assertEqual(2, cms.read('score.cvs'))
 
+
+        def test_read_3(self):
+            m_open = mock_open(read_data="1,±è±è±è,90,70,80\n2,³ª³ª³ª,80,95,90\n3,´Ù´Ù,80,40,50")
+
+        with patch('car_management_ststem.open', m_open):
+            cms = ScoreManagementSystem()
+            self.assertEqual(2, cms.read('score.cvs'))
 
 
 
